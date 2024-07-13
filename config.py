3dca793cbe64c88867a3f55c2b01814a42201526
Александр Быков <150475371+Jimaet@users.noreply.github.com>
2024-07-13 15:49:27 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-API_TOKEN = '7327318190:AAHww3x8UmlPGy9392R6tXwRxTl-8VwaLCw'
+API_TOKEN = ''
 CHANNEL_ID_1 = -1002162179383  # ID первого канала
 CHANNEL_ID_2 = -1002177082543  # ID второго канала
 CHANNEL_USERNAME_1 = 'world_game_news_official'  # Юзернейм первого канала
